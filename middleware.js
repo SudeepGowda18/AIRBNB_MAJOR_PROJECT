27ca1078cc2e02e4d65d3359df8e9e419c5a507d
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-23 21:54:55 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Listing = require("./models/listings");
+const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
