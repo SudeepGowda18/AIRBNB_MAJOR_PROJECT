80ae3941f4d52ea7f66dc3f490877d99eb435cc3
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-23 21:55:09 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 const Listing = require("./models/listings");
 const Review = require("./models/review");
-const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
