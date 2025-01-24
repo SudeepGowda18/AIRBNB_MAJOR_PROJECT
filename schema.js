1bb7c145e07b3dfb6eac54307e11c9c8df960bb0
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-24 20:13:38 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 module.exports.listingSchema = Joi.object({
 
-    listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
