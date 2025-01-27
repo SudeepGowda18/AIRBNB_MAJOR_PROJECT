6d14b8b4f0dc0b185d6106a2db2e961a230b26ca
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-27 22:32:04 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -12,7 +12,6 @@ module.exports.isLoggedIn = (req, res, next) => {
   next();
 };
 
-module.exports.saveRedirectUrl = (req, res, next) => {
   if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
